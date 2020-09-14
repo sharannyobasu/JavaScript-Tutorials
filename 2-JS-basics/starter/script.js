1a05a01a34bca47fc5a15d85ff7f79005a2b6f4d
Sharannyo <yobasu2015@gmail.com>
2020-09-14 22:45:24 +0530

    Added a new JS file

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,26 @@
+var firstname = 'John';
+console.log(firstname);
+
+var lastname = 'Smith';
+var age = 25;
+
+var fullage = true;
+console.log(fullage);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+var $3ee = 78;
+console.log($3ee);
+
+//var function = 23; -> Shows error in console
+
+var job, married;
+job = 'teacher';
+married = false;
+
+var lastname = prompt('What is his last name ?');
+console.log('His name is ' + firstname + ' ' + lastname);
