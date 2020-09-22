18ed513a85ff04b68f8f6d859a7f263594cbb6f2
Sharannyo <yobasu2015@gmail.com>
2020-09-22 21:08:59 +0530

    Updated Pig Game code

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,11 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var scores, roundScore;
+scores = [0, 0];
+roundScore = 0;
+activeplayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector('#score-0').textContent = dice;
